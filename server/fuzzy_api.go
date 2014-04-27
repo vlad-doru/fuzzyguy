e099da47415f0dbfeafaed6b911325dcb9351e68
Vlad-Doru Ion <vlad.doru@gmail.com>
2014-04-27 22:37:57 +0300

    Reorganized the structure so it becomes even more modular so that we can easily add tests

--- server/fuzzy_api.go
+++ server/fuzzy_api.go
@@ -1,12 +1,10 @@
 package server
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/vlad-doru/fuzzyguy/fuzzy"
 	"net/http"
-	"os/exec"
 	"strconv"
 )
 
@@ -154,37 +152,4 @@ func FuzzyHandler(w http.ResponseWriter, r *http.Request) {
 	default:
 		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 	}
-}
-
-func StatsHandler(w http.ResponseWriter, r *http.Request) {
-
-	json_response, err := json.Marshal(Fuzzy.stats)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-
-	}
-	fmt.Fprintf(w, string(json_response))
-	w.Header().Set("Content-Type", "application/json")
-}
-
-func TestHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
-		return
-	}
-	parameters, valid := RequireParameters([]string{"testsize", "distance", "results"}, w, r)
-	if !valid {
-		return
-	}
-	cmd := exec.Command("python", "test/test.py", parameters["testsize"], parameters["distance"], parameters["results"])
-	var out bytes.Buffer
-	cmd.Stderr = &out
-	err := cmd.Run()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	fmt.Fprintf(w, out.String())
-	w.Header().Set("Content-Type", "application/json")
 }
